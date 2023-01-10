2c6b64979c733b8511f6eebb3e0a048c3c2214ec
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-10 13:46:55 +0200

    add check_out_widget.dart and apply it to cart_screen.dart

--- lib/presentation_layer/screens/btm_nav_bar_screen/cart_screen/cart_screen.dart
+++ lib/presentation_layer/screens/btm_nav_bar_screen/cart_screen/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import '../../../../core/style/color_manager.dart';
 import '../../../provider/cart_provider.dart';
+import '../../../widgets/check_out_widget.dart';
 import '../../../widgets/empty_screen.dart';
 import '../../../widgets/global_methode.dart';
 import '../../../widgets/text_widget.dart';
@@ -58,7 +59,7 @@ class CartScreen extends StatelessWidget {
             ),
             body: Column(
               children: [
-                _checkOut(context: context),
+                CheckOutWidget(),
                 Expanded(
                   child: ListView.builder(
                     itemBuilder: (ctx, index) {
@@ -73,45 +74,5 @@ class CartScreen extends StatelessWidget {
               ],
             ),
           );
-  }
-
-  Widget _checkOut({
-    required BuildContext context,
-  }) {
-    Size size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        width: double.infinity,
-        height: size.height * 0.1,
-        child: Row(
-          children: [
-            Material(
-              color: ColorManager.primary,
-              borderRadius: BorderRadius.circular(10),
-              child: TextButton(
-                onPressed: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Checkout',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-            const Spacer(),
-            FittedBox(
-              child: TextWidget(
-                text: 'Total : \$${5.toStringAsFixed(2)}',
-                color: Colors.black,
-                textSize: 18,
-                isTitle: true,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
