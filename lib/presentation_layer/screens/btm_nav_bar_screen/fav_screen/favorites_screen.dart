f3f0606684e892beb37cf292f58253ffa10ee265
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-10 13:41:35 +0200

    add appBar to favorites_screen.dart

--- lib/presentation_layer/screens/btm_nav_bar_screen/fav_screen/favorites_screen.dart
+++ lib/presentation_layer/screens/btm_nav_bar_screen/fav_screen/favorites_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+import '../../../../core/style/color_manager.dart';
+import '../../../widgets/global_methode.dart';
+import '../../../widgets/text_widget.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
@@ -6,8 +11,34 @@ class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('FavoritesScreen'),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0.0,
+        backgroundColor: ColorManager.backGround,
+        title: TextWidget(
+          text: 'Cart(2)',
+          color: ColorManager.primary,
+          textSize: 22,
+          isTitle: true,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              GlobalMethode.warningDialog(
+                title: 'Empty your cart',
+                subtitle: 'Are you sure?',
+                fct: () async {
+                  /// TODO delete all favorites items from favoritesScreen
+                },
+                context: context,
+              );
+            },
+            icon: Icon(
+              IconlyBroken.delete,
+              color: ColorManager.primary,
+            ),
+          ),
+        ],
       ),
     );
   }
