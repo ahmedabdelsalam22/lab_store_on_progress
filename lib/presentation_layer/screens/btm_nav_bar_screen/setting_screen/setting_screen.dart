d0314e165ed92c29f2d261bc78192f58565fc954
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-11 11:36:49 +0200

    add Technical support ListTiles in column in SettingScreen

--- lib/presentation_layer/screens/btm_nav_bar_screen/setting_screen/setting_screen.dart
+++ lib/presentation_layer/screens/btm_nav_bar_screen/setting_screen/setting_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 
+import '../../../../core/style/color_manager.dart';
 import '../../../widgets/text_widget.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -9,73 +10,95 @@ class SettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RichText(
-                text: TextSpan(
-                    text: 'Hi,   ',
-                    style: const TextStyle(
-                        color: Colors.cyan,
-                        fontSize: 27,
-                        fontWeight: FontWeight.bold),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: 'user',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 25,
-                              fontWeight: FontWeight.normal),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print('my name is pressed');
-                            }),
-                    ]),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextWidget(
-                text: 'user email',
-                color: Colors.black,
-                textSize: 18,
-                // isTitle: true,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Divider(
-                thickness: 2,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              _listTiles(
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: TextWidget(
+            text: 'Setting',
+            color: ColorManager.primary,
+            textSize: 22,
+            isTitle: true,
+          ),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                      text: 'Hi,   ',
+                      style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'user',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 25,
+                                fontWeight: FontWeight.normal),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print('my name is pressed');
+                              }),
+                      ]),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextWidget(
+                  text: 'user email',
+                  color: Colors.black,
+                  textSize: 18,
+                  // isTitle: true,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _listTiles(
                   title: 'Address',
                   subtitle: 'address',
                   icon: IconlyBold.profile,
                   onPressed: () async {
                     /// TODO
                   },
-                  color: Colors.black),
-              _listTiles(
+                  color: Colors.black,
+                ),
+                _listTiles(
+                    title: 'Technical support',
+                    icon: Icons.help,
+                    color: Colors.black,
+                    onPressed: () {
+                      ///Navigate to login screen
+                    }),
+                _listTiles(
                   title: 'Logout',
                   icon: IconlyBold.logout,
                   color: Colors.black,
                   onPressed: () {
                     ///Navigate to login screen
-                  }),
-            ],
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  //
 
   Widget _listTiles(
       {required String title,
